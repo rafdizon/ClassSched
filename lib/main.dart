7bf7a646a25c6ae29dd419905265850aabbc7c26
rafdizon <rafaeldizon9@gmail.com>
2025-04-10 02:48:54 +0800

    Public

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:class_sched/auth_gate.dart';
 import 'package:class_sched/services/notifications_student_service.dart';
 import 'package:class_sched/services/settings_utils.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:logger/logger.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:timezone/timezone.dart' as tz;
@@ -10,12 +11,16 @@ import 'package:timezone/data/latest.dart' as tz;
 import 'dart:io';
 import 'package:window_manager/window_manager.dart';
 
-const String ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3aGRlYnFmb293cWl3bnZiemRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc1NjEwMzIsImV4cCI6MjA1MzEzNzAzMn0.Fj6ZlvPJYZcLmFymYa21frwW2jLGb7HfRov-FhpJxgk";
-const String URL = "https://dwhdebqfoowqiwnvbzdo.supabase.co";
-const String ADMIN_EMAIL = "classschedspusm@gmail.com";
+late String ANON_KEY;
+late String URL;
+late String ADMIN_EMAIL;
 
 void main() async {
+  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
+  ANON_KEY = dotenv.env['ANON_KEY']!;
+  URL = dotenv.env['URL']!;
+  ADMIN_EMAIL = dotenv.env['ADMIN_EMAIL']!;
   await Supabase.initialize(
     anonKey: ANON_KEY,
     url: URL,
